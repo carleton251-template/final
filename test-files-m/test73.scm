15c9524052e8f28e6d01afcd6b638b55111edac4
Dave Musicant (home machine) <dmusicant@carleton.edu>
2022-12-31 11:28:05 -0600

    w23 updates

--- test-files-m/test73.scm
+++ test-files-m/test73.scm
@@ -1,1 +1,4 @@
-(or #t (if (= (/ 2 0) 3) #f #t))
+; Note: this is testing whether you've implemented
+; and properly as a special form! (You don't need
+; division implemented to pass this test.)
+(and #f (if (= (/ 2 0) 3) #f #t))
